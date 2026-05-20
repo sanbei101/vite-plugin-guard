465d50d0298d52a9c377b436c690098c67757dce
sanbei101 <sanbei101@outlook.com>
2026-05-20 17:03:08 +0800

    更新配置文件，添加 Vitest 测试配置，启用浏览器测试支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { playwright } from "@vitest/browser-playwright";
+import { defineConfig } from "vitest/config";
 
 import Guard from "./src/Guard";
 
 export default defineConfig({
   plugins: [vue(), Guard({ password: "1234" })],
+  test: {
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
+      headless: true,
+    },
+  },
 });
